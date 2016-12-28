6e250361a9468511bb52411d4f0b85392adc215a
zhaoyoushuai <zhaoyoushuai@meituan.com>
2016-12-28 15:19:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <example :msg="msg" @showMsg="showInfo" @hideMsg="hideInfo">
+      <template slot="title">Hello Vue2.x</template>
+    </example>
+    <p :style="{ display: show }">This is a msg in father but emited by child</p>
+  </div>
+</template>
+
+<script>
+import example from './components/example.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'My first component',
+      show: 'none'
+    }
+  },
+  components: {
+    example
+  },
+  methods: {
+    showInfo () {
+      this.show = 'block'
+    },
+    hideInfo () {
+      this.show = 'none'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/example.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <p @click="emitParent">{{ msg }}</p>
+    <p @click="emitHide">Hide Msg</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zys',
+  props: {
+    msg: String,
+    required: true,
+    default: 'hello'
+  },
+  methods: {
+    emitParent () {
+      this.$emit('showMsg')
+    },
+    emitHide () {
+      this.$emit('hideMsg')
+    }
+  }
+}
+</script>

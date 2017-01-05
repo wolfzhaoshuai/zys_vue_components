1a4c5a9dea88f5460a70049280924b10d0da64c9
zhaoyoushuai <zhaoyoushuai@meituan.com>
2017-01-05 19:18:11 +0800

    自定义vue插件续

--- src/zys.js
+++ src/zys.js
@@ -4,16 +4,10 @@ import ZysButton from './components/button.vue'
 import Message from './components/message'
 
 const install = function (Vue) {
-  // 待办
-  /* Example.install(Vue)
-  Trans.install(Vue)
-  ZysButton.install(Vue)
-  Message.install(Vue) */
-
   // 注册到原型事件后可以按this.$message方式使用
   Vue.prototype.$message = Message
 
-  // 模块重命名
+  // 注册模块
   Vue.component('example', Example)
   Vue.component('zysButton', ZysButton)
 }
